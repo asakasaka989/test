b0cb97bf1ef835aeb12829c5ea53939ed91babd3
misakiinoue <93175675+misakiinoue@users.noreply.github.com>
2023-06-23 19:59:41 +0900

    Update main.dart

--- submit/01_井上/Q2/main.dart
+++ submit/01_井上/Q2/main.dart
@@ -7,3 +7,17 @@ void main() {
     print('\n');
   }
 }
+//main2.dart
+void main() {
+  List<int> array1 = [2, 6, 9, 8];
+  List<int> array2 = [1, 1, 1, 2];
+
+  print("降順に並び替えた結果:");
+  print(sortDescending(array1).join());
+  print(sortDescending(array2).join());
+}
+
+List<int> sortDescending(List<int> array) {
+  array.sort((a, b) => b.compareTo(a));
+  return array;
+}
